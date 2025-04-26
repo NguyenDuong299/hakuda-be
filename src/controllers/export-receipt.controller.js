1ce5701cb6caaa3f79f385ed1760b384cc3f3379
duongnt <duongnt@vikings.com.vn>
2025-04-26 18:32:50 +0700

    [feat] update be

--- src/controllers/export-receipt.controller.js
+++ src/controllers/export-receipt.controller.js
@@ -3,8 +3,14 @@ const ExportReceipt = require("../models/export-receipt.model");
 const exportReceiptController = {
   getAllExportReceipt: async (req, res) => {
     try {
-      const exportReceipts = await ExportReceipt.getAllExportReceipt();
-      res.json(exportReceipts);
+      const page = parseInt(req.query.page) || 1;
+      const limit = 10;
+      const offset = (page - 1) * limit;
+      const search = req.query.search || "";
+
+      const totalExportReceipt = await ExportReceipt.getTotalExportReceipt(search);
+      const exportReceipts = await ExportReceipt.getAllExportReceipt(limit, offset, search);
+      res.json({ page, totalExportReceipt, exportReceipts });
     } catch (err) {
       res.status(500).json({ error: err.message });
     }
@@ -20,28 +26,44 @@ const exportReceiptController = {
   },
   createExportReceipt: async (req, res) => {
     try {
-      const exportReceipt = await ExportReceipt.createExportReceipt(req.body);
-      res.status(201).json(exportReceipt);
+      const { order_id, export_date, total_amount, user_id, status, export_receipt_details = [] } = req.body;
+
+      if (!order_id || !export_date || !total_amount || !user_id || !status || export_receipt_details.length === 0) {
+        return res.status(400).json({ message: "Thiếu thông tin biên lai!" });
+      }
+      const result = await ExportReceipt.createExportReceipt({
+        order_id,
+        export_date,
+        total_amount,
+        user_id,
+        status,
+        export_receipt_details,
+      });
+      res.status(201).json(result);
     } catch (err) {
-      res.status(500).json({ error: err.message });
+      console.error("Error creating order:", err);
+      res.status(500).json({ message: "Lỗi khi tạo biên lai.", error: err.message });
     }
   },
   updateExportReceipt: async (req, res) => {
     try {
       const { id } = req.params;
-      const exportReceipt = await ExportReceipt.updateExportReceipt(id, req.body);
-      res.json(exportReceipt);
+      const { status } = req.body;
+      const result = await ExportReceipt.updateExportReceipt(id, {
+        status,
+      });
+      res.json({ message: "Chỉnh sửa biên lai thành công!", result });
     } catch (err) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({ message: "Lỗi khi cập nhật biên lai.", error: err.message });
     }
   },
   deleteExportReceipt: async (req, res) => {
     try {
       const { id } = req.params;
       await ExportReceipt.deleteExportReceipt(id);
-      res.json({ message: "Xóa hóa đơn hàng thanh cong!" });
+      res.json({ message: "Xóa biên lai thành công!" });
     } catch (err) {
-      res.status(500).json({ error: err.message });
+      res.status(500).json({ message: "Lỗi khi xóa biên lai.", error: err.message });
     }
   },
 };
--- src/models/export-receipt.model.js
+++ src/models/export-receipt.model.js
@@ -18,15 +18,14 @@ const exportReceiptModel = {
         p.createdAt,
         p.updatedAt,
         JSON_ARRAYAGG(
-          IF(
-            pi.product_id IS NOT NULL AND pi.quantity IS NOT NULL AND pi.export_price IS NOT NULL,
             JSON_OBJECT(
+              'id', pi.id,
               'product_id', pi.product_id,
               'quantity', pi.quantity,
-              'export_price', pi.export_price
-            ),
-            NULL
-          )
+              'export_price', pi.export_price,
+              'createdAt', pi.createdAt,
+              'updatedAt', pi.updatedAt
+            )
         ) AS export_receipt_details
       FROM export_receipts p
       LEFT JOIN export_receipt_details pi ON p.id = pi.export_receipt_id
@@ -37,13 +36,8 @@ const exportReceiptModel = {
 
       connection.query(sql, [searchQuery, searchQuery, limit, offset], (err, results) => {
         if (err) return reject(err);
-
         results.forEach((item) => {
-          try {
-            item.export_receipt_details = JSON.parse(item.export_receipt_details);
-            if (!Array.isArray(item.export_receipt_details)) item.export_receipt_details = [];
-            item.export_receipt_details = item.export_receipt_details.filter((i) => i !== null);
-          } catch {
+          if (!item.export_receipt_details || item.export_receipt_details.length === 0 || item.export_receipt_details.every((item) => item === null)) {
             item.export_receipt_details = [];
           }
         });
@@ -53,39 +47,80 @@ const exportReceiptModel = {
     });
   },
 
-  createExportReceipt: (exportReceipt) => {
+  getTotalExportReceipt: () => {
     return new Promise((resolve, reject) => {
-      const sql = `INSERT INTO export_receipts SET ?`;
-      connection.query(sql, exportReceipt, (err, results) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
+      const sql = `
+            SELECT COUNT(*) AS total 
+            FROM export_receipts 
+          `;
+
+      connection.query(sql, (err, results) => {
+        if (err) return reject(err);
+        resolve(results[0].total);
+      });
+    });
+  },
+
+  createExportReceipt: (orderData) => {
+    return new Promise((resolve, reject) => {
+      const { order_id, export_date, total_amount, user_id, status, export_receipt_details = [] } = orderData;
+
+      if (!export_receipt_details.length) {
+        return reject(new Error("Order must contain at least one item."));
+      }
+
+      const insertOrderSql = `
+      INSERT INTO export_receipts (order_id, export_date, total_amount, user_id, status)
+      VALUES (?, ?, ?, ?, ?)
+    `;
+
+      connection.beginTransaction((err) => {
+        if (err) return reject(err);
+
+        connection.query(insertOrderSql, [order_id, export_date, total_amount, user_id, status], (err, result) => {
+          if (err) return connection.rollback(() => reject(err));
+
+          const exportReceiptId = result.insertId;
+
+          const insertItemsSql = `
+            INSERT INTO export_receipt_details (export_receipt_id, product_id, quantity, export_price)
+            VALUES ?
+          `;
+          const itemsValues = export_receipt_details.map((item) => [exportReceiptId, item.product_id, item.quantity, item.export_price]);
+          connection.query(insertItemsSql, [itemsValues], (err) => {
+            if (err) return connection.rollback(() => reject(err));
+            connection.commit((err) => {
+              if (err) return connection.rollback(() => reject(err));
+              resolve({ message: "Export receipt created successfully", exportReceiptId, export_receipt_details });
+            });
+          });
+        });
       });
     });
   },
   updateExportReceipt: (id, exportReceipt) => {
+    const newExportReceipt = {
+      ...exportReceipt,
+      updatedAt: new Date(),
+    };
+
     return new Promise((resolve, reject) => {
-      const sql = `UPDATE export_receipts SET ? WHERE id = ?`;
-      connection.query(sql, [exportReceipt, id], (err, results) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
+      connection.query("UPDATE export_receipts SET ? WHERE id = ?", [newExportReceipt, id], (err, results) => {
+        if (err) return reject(err);
+        resolve(results);
       });
     });
   },
   deleteExportReceipt: (id) => {
     return new Promise((resolve, reject) => {
-      const sql = `DELETE FROM export_receipts WHERE id = ?`;
-      connection.query(sql, [id], (err, results) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(results);
-        }
+      const deleteExportReceiptSql = `
+        DELETE FROM export_receipts
+        WHERE id = ?
+      `;
+
+      connection.query(deleteExportReceiptSql, [id], (err, result) => {
+        if (err) return reject(err);
+        resolve(result);
       });
     });
   },
